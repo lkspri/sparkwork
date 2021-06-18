185c41a89938a332aabb73a5ed636d9483d1e593
priya <lakshmi.pri@gmail.com>
2021-06-18 16:25:01 +0000

    Added sparkuser2

---
+++ Lab01.scala
@@ -0,0 +1,24 @@
+package sparkcore
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+object Lab01 {
+  
+  def main(args:Array[String])=
+  {
+    println("Welcome to Spark....")
+    
+    val sc = new SparkContext(new SparkConf().setAppName("First-Spark").setMaster("local"))
+    
+    val lst = List(10,20,30)
+    
+    val rdd = sc.parallelize(lst)
+    
+    val total = rdd.sum()
+    
+    print(s"Total value:$total")
+ 
+  }
+}
